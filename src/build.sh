6d053c3cf0afae779351bc4fea77921933878539
Xliff <clifton.wood@gmail.com>
2021-05-06 12:17:51 -0400

    - Changes the location and name of the tree-helper library

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 gcc glib-support.c `pkg-config --cflags gobject-2.0` -shared -fPIC -o glib-support.so `pkg-config --libs gobject-2.0`
-gcc tree-helper.c -shared -fPIC -o trees-helper.so
+gcc tree-helper.c -shared -fPIC -o tree-helper.so
