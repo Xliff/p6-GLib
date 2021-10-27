08e2ecb3a8fb49bfa9c8213530f49b0c525b55ef
Xliff <clifton.wood@gmail.com>
2021-10-27 18:10:39 -0400

    - Adds prerequisite installation script.

---
+++ install-prereqs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zef install --serial Config::INI Cairo CompUnit::Util Config::INI Data::Dump::Tree Dependency::Sort LibXML Method::Also NativeHelpers::Blob Pluggable Color Dependency::Sort File::Find Inline::Perl5 IO::Socket::SSL LWP::Simple RandomColor Text::Table::Simple JsonC
