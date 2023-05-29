2999a34d6efdf3a342890e65e64da01961addea4
Xliff <clifton.wood@gmail.com>
2023-05-29 01:54:30 -0400

    - Updated install-prereqs.sh script

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-zef install --serial Config::INI Cairo CompUnit::Util Config::INI Data::Dump::Tree Dependency::Sort LibXML Method::Also NativeHelpers::Blob Pluggable Color Dependency::Sort File::Find Inline::Perl5 IO::Socket::SSL LWP::Simple RandomColor Text::Table::Simple 
+zef install --serial Config::INI Cairo CompUnit::Util Config::INI Data::Dump::Tree Dependency::Sort LibXML Method::Also NativeHelpers::Blob Pluggable Color Dependency::Sort File::Find Inline::Perl5 IO::Socket::SSL LWP::Simple RandomColor Text::Table::Simple Terminal::ANSI::OO IO::Capture::Simple
 
 # Currently replaced with JSON::Fast because it will not install as of 7/10/2022
 #zef install JsonC
