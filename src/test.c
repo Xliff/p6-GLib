2f7c9457c26a8e46763a99ffed242f4dd94e10ab
Xliff <clifton.wood@gmail.com>
2023-05-29 01:56:02 -0400

    - Updated src/test.c

--- src/test.c
+++ src/test.c
@@ -1,7 +1,11 @@
+#include <glib.h>
 #include <glib-object.h>
+#include <gio/gio.h>
+#include <stdio.h>
 
 void main (int argc, char **argv) {
-	printf("From src: %ld\n", 20 << 2);
+	printf("From src: %ld\n", (long)(20 << 2));
+	printf("GObjectPrivate size: %ld\n", sizeof(GOutputStream));
 	printf("GObject type: %ld\n", g_object_get_type() );
 	printf("GObjectClass: %ld\n", sizeof(GObjectClass));
 	printf("GTypeClass: %ld\n", sizeof(GTypeClass));
