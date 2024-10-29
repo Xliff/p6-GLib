b299480a4629230c67327d1e081dbb69a913e189
Clifton Wood <clifton.wood@gmail.com>
2024-10-28 20:25:03 -0400

    - Updated shared lib build script

--- t/build-shared-libs.sh
+++ t/build-shared-libs.sh
@@ -1,1 +1,1 @@
-gcc 00-struct-sizes.c -Wno-deprecated-declarations -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include --shared -lglib-2.0
+gcc 00-struct-sizes.c -Wno-deprecated-declarations -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include --shared -lglib-2.0 -o 00-struct-sizes.so
